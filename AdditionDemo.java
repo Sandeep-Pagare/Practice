1a8a4fcf3d73c89425006085912de09b6267efc4
Sandeep Pagare <105290093+Sandeep-Pagare@users.noreply.github.com>
2022-05-11 19:24:13 +0530

    Update AdditionDemo.java

--- AdditionDemo.java
+++ AdditionDemo.java
@@ -3,6 +3,6 @@ class AdditionDemo
 	public static void main(String abc[])
 	{
 		int a=10, b=20;	
-		System.out.println("Addition is "+(a+b));
+		System.out.println("Addition of a and b is "+(a+b));
 	}
 }
